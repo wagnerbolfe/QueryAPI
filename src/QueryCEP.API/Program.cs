50fcd971bbbd0b81d943075ca90a6d63a15cfede
Wagner Bolfe <shaper@live.com>
2023-05-29 19:14:20 -0300

    last update

--- src/QueryCEP.API/Program.cs
+++ src/QueryCEP.API/Program.cs
@@ -80,11 +80,8 @@ builder.Services.AddHttpClient();
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 //app.UseHttpsRedirection();
 
